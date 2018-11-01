124ab5540d9358b8d1e60bf262ffcbfd587c9c6d
rudra <es.rudra.chandra@gmail.com>
2018-11-01 14:40:14 +0545

    mongo db added

---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+
+import mongoose from 'mongoose';
+
+import constants from './constants';
+
+// Remove the warning with Promise
+mongoose.Promise = global.Promise;
+
+// Connect the db with the url provide
+try {
+  mongoose.connect(constants.MONGO_URL);
+} catch (err) {
+  mongoose.createConnection(constants.MONGO_URL);
+}
+
+mongoose.connection
+  .once('open', () => console.log('MongoDB Running'))
+  .on('error', e => {
+    throw e;
+  });
+
+
+
+//   ./mongod --dbpath=/Users/mac/Documents/data running mongod with mongo bd location 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import express from 'express';
-
+import constants from './config/constants.js';
+import "./config/database";
+import middlewaresConfig from "./config/middlewares"
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, err =>{
+middlewaresConfig(app);
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(constants.PORT, err =>{
     if(err){
         throw err;
     }else {
-        console.log(`server running on ${PORT}`)
+        console.log(`server running on ${constants.PORT}`)
     }
 })
